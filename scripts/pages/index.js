abeef592a81d7d05b3747e58a08eeef8f0dd2f59
John Park <johnbuspark@gmail.com>
2023-11-07 23:42:14 -0800

    Fixed authentication; added dynamic user document updating feature

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,5 +1,11 @@
 
 
-function login(){
+function loginPage(){
+    window.localStorage.setItem("authStyle", "login");
+    window.location.href = "./login.html";
+}
+
+function signupPage(){
+    window.localStorage.setItem("authStyle", "signup");
     window.location.href = "./login.html";
 }
--- scripts/pages/skeleton.js
+++ scripts/pages/skeleton.js
@@ -5,11 +5,18 @@
 /** Inserts default navbar 
  * 
  */
-function insertNavbarDefault(){
+function insertNavbarDefault() {
     $("#put-navbar-here").load("/components/navbar.html")
 }
 
-function insertFooterDefault(){
+function insertFooterDefault() {
     $("#put-footer-here").load("/components/footer.html")
 }
 
+function insertLogin() {
+    if ((window.localStorage.getItem("authStyle") ?? "login") === "login"){
+        $("#put-login-here").load("/components/login.html");
+    }else{
+        $("#put-login-here").load("/components/signup.html");
+    }
+}
