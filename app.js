5de627a2705855d3fb463906aa44ae720934a9eb
John Park <johnbuspark@gmail.com>
2023-11-16 23:39:20 -0800

    committing everything

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ app.use(express.json());
 const fs = require("fs");
 
 // Virtual Paths
-app.use("/js", express.static("./app/js"));
-app.use("/css", express.static("./public/css"));
-app.use("/img", express.static("./public/img"));
-app.use("/font", express.static("./public/font"));
-app.use("/components", express.static("./public/components"));
+app.use("/js", express.static("app/js"));
+app.use("/css", express.static("public/css"));
+app.use("/img", express.static("public/img"));
+app.use("/font", express.static("public/font"));
+app.use("/components", express.static("public/components"));
 
 app.get("/", function (req, res) {
     let doc = fs.readFileSync("./app/html/index.html", "utf8");
---
+++ app/js/modules/global.js
@@ -0,0 +1,28 @@
+export {htmlAlias, domain, ajaxGet};
+// Alias for html
+    
+let htmlAlias = 
+{
+    //entry.html
+    entry:"entry",
+    index:"index"
+}
+
+// variables
+let domain = "http://localhost:8000";
+
+
+// Functions
+let ajaxGet = function ajaxGET(url, callback){
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
+            callback(this.responseText);
+        }else{
+            console.log(this.status);
+        }
+    }
+    xhr.open("GET", url);
+    xhr.send(); 
+}
+
