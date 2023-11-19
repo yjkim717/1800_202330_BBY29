5a733975bdea8f17c93ca64f7fb635f1796f45e3
John Park <johnbuspark@gmail.com>
2023-11-19 02:37:20 -0800

    made namespace for functions

--- app/js/pages.js
+++ app/js/pages.js
@@ -1,109 +1,149 @@
 
 
 
-
-/**
- * Adds event listeners to login and sign up buttons. It 
- * redirects window to login.html, which 
- */
-function doAllIndex() {
-    insertNavbar();
-    insertFooter();
-    let login = document.getElementById(components.login.elements.loginButton);
-    let signup = document.getElementById(components.signup.elements.signupButton);
-    login.addEventListener("click", function (e) {
-        window.location.href = domain + "/entry?authStyle=login";
-    })
-    signup.addEventListener("click", function (e) {
-        window.location.href = domain + "/entry?authStyle=signup";
-    })
-}
-
-/**
- * Insert login/signup 
- */
-function doAllEntry() {
-    let url = new URL(window.location.href);
-    let search_param = url.searchParams;
-    let login = document.getElementById(components.login.placeholder);
-    if (search_param.get("authStyle") === "login") {
-        ajaxGET("/components/login.html", function (data) {
-            login.innerHTML = data;
-            completeButton(components.login.elements.loginSubmitButton, components.login.container);
+let indexFunc = {
+    doAllIndex: function () {
+        insertNavbar();
+        insertFooter();
+        let login = document.getElementById(components.login.elements.loginButton);
+        let signup = document.getElementById(components.signup.elements.signupButton);
+        login.addEventListener("click", function (e) {
+            window.location.href = domain + "/entry?authStyle=login";
         })
-    } else if (search_param.get("authStyle") === "signup") {
-        ajaxGET("/components/signup.html", function (data) {
-            login.innerHTML = data;
-            addButtonToMap(components.signup.elements.signupSubmitButton, components.signup.container);
-        });
-    } else {
-        console.log("Failed");
+        signup.addEventListener("click", function (e) {
+            window.location.href = domain + "/entry?authStyle=signup";
+        })
     }
 }
 
-/**
- * Helper function of doAllEntry()
- */
-function completeButton(button, authStyle) {
-    let userInfo = document.getElementById(authStyle);
-    if (userInfo) {
-        let signupSubmitButton = document.getElementById(button);
-        if (signupSubmitButton) {
-            signupSubmitButton.addEventListener("click", function (e) {
-                
-                if (button === "loginSubmitButton"){
-                    if (login()){
-                        window.location.href = domain + "/map";
-                    }else{
-                        alert("Login failed");
-                    }
-                }else if(button === "signupSubmitButton"){
-                    if (signup()){
-                        window.location.href = domain + "/map";
-                    }else{
-                        alert("Signup failed");
-                    }
-                }
-                
+let entryFunc = {
+    doAllEntry: function () {
+        let url = new URL(window.location.href);
+        let search_param = url.searchParams;
+        let login = document.getElementById(components.login.placeholder);
+        if (search_param.get("authStyle") === "login") {
+            ajaxGET("/components/login.html", function (data) {
+                login.innerHTML = data;
+                entryFunc.completeButton(components.login.elements.loginSubmitButton, components.login.container);
+            })
+        } else if (search_param.get("authStyle") === "signup") {
+            ajaxGET("/components/signup.html", function (data) {
+                login.innerHTML = data;
+                entryFunc.addButtonToMap(components.signup.elements.signupSubmitButton, components.signup.container);
             });
         } else {
-            console.log("signup submit button not found");
+            console.log("Failed");
         }
-    } else {
-        console.log("signup element not found");
+    },
+    completeButton: function (button, authStyle) {
+        let userInfo = document.getElementById(authStyle);
+        if (userInfo) {
+            let signupSubmitButton = document.getElementById(button);
+            if (signupSubmitButton) {
+                signupSubmitButton.addEventListener("click", function (e) {
+
+                    if (button === "loginSubmitButton") {
+                        if (login()) {
+                            window.location.href = domain + "/map";
+                        } else {
+                            alert("Login failed");
+                        }
+                    } else if (button === "signupSubmitButton") {
+                        if (signup()) {
+                            window.location.href = domain + "/map";
+                        } else {
+                            alert("Signup failed");
+                        }
+                    }
+
+                });
+            } else {
+                console.log("signup submit button not found");
+            }
+        } else {
+            console.log("signup element not found");
+        }
     }
 }
 
-/**
- * PopList
- */
-function openList() {
-    let popupList = document.getElementById(components.restaurantList.placeholder);
-    let searchButton = document.getElementById(components.search.elements.searchButton);
-    searchButton.addEventListener("click", function (e) {
-        ajaxGET("/components/" + htmlAlias.restaurantList + ".html", function (data) {
-            //Grab element in popup.html to check if its dom is loaded
+let mapFunc = {
+    doAllMap: function () {
+        let popupList = document.getElementById(components.restaurantList.placeholder);
+        let searchButton = document.getElementById(components.search.elements.searchButton);
+        searchButton.addEventListener("click", function (e) {
+            ajaxGET("/components/" + htmlAlias.restaurantList + ".html", function (data) {
+                //Grab element in popup.html to check if its dom is loaded
 
-            popupList.innerHTML = data;
-            let restaurantTemplate = document.getElementById(components.restaurantList.elements.restaurantTemplate);
-            if (restaurantTemplate) {
-                db.collection("restaurants").get().then((querySnapshot) => {
-                    querySnapshot.forEach((doc) => {
-                        //template elements don't have child nodes until you grab its ".content"
-                        let rest = restaurantTemplate.content.cloneNode(true);
-                        let restData = doc.data();
-                        rest.getElementById(`${components.restaurantList.elements.restaurantName}`).innerHTML = restData.name;
-                        rest.getElementById(`${components.restaurantList.elements.checkbox}`).attributes.dataId = doc.id;
+                popupList.innerHTML = data;
+                let restaurantTemplate = document.getElementById(components.restaurantList.elements.restaurantTemplate);
+                if (restaurantTemplate) {
+                    db.collection("restaurants").get().then((querySnapshot) => {
+                        querySnapshot.forEach((doc) => {
+                            //template elements don't have child nodes until you grab its ".content"
+                            let rest = restaurantTemplate.content.cloneNode(true);
+                            let restData = doc.data();
+                            rest.getElementById(`${components.restaurantList.elements.restaurantName}`).innerHTML = restData.name;
+                            rest.getElementById(`${components.restaurantList.elements.checkbox}`).attributes.dataId = doc.id;
 
-                        document.getElementById(components.restaurantList.elements.restaurantContainer).append(rest);
+                            document.getElementById(components.restaurantList.elements.restaurantContainer).append(rest);
 
+                        });
                     });
-                });
-            }
+                    document.getElementById("requestButton").addEventListener("click", function (e) {
+                        mapFunc.lineup();
+                    });
+                }
+
+            });
+        })
+    },
+    lineup: function () {
+        let user = firebase.auth().currentUser;
+
+        if (!user) {
+            console.log("NOT USER SIGNEDD IN");
+            return;
+        }
+        let posterID = user.uid;
+        let number = document.getElementById("numberOfPeople").value;
+
+        //TODO: Grab array of restaurants selected from signup popup
+        // TODO: Loop the bottom code for each restaurant
+        $("input:checked.selectRestaurant").each((index, element) => {
+            const restaurantID = $(element).attr("dataId");
+            //TODO: add to requestlist of user the id of newly made request object 
+            db.collection("signup").add({
+                posterID,
+                restaurantID,
+                number,
+                status: true
+            })
 
         });
-    })
-    
+        db.collection("signup").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                let use = db.collection("users").doc(user.uid);
+                use.update({
+                    myrequest: firebase.firestore.FieldValue.arrayUnion(doc.id)
+                }).then(() => {
+                    console.log("success");
+                }).catch(() => {
+                    console.log("fail");
+                });
+            })
+        })
+    }
+}
+
+
+
+//TODO: when readSignup call change the status to false and remove from db
+function readSignup() {
+    console.log("write signup");
+    let posterID = db.collection("users").doc(user.uid);
+    let restaurantID = db.collection("restaurants").doc(restaurant.rid);
+    let number = document.getElementById("numberOfPeople").value;
+    let status = false;
 }
 
 
